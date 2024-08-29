0c498f31d4ac8bfa48d8b21456dc9064aad1ffc1
amgtechpro <amgtechpro@gmail.com>
2024-08-29 10:28:55 +0800

    Added total calories and checked that the code is working fine in the virtual environment.

--- meal-calculator.py
+++ meal-calculator.py
@@ -15,8 +15,13 @@ class Food:
         self.carbs = carbs
         self.fat = fat
         self.total_grams = protein + carbs + fat
+        self.total_calories = (protein * 4) + (carbs * 4) + (fat * 9)
         
     def __repr__(self):
-        return f"{self.name} contains {self.protein}grams of protein, {self.carbs}grams of carbs, and {self.fat}grams of fat. The total amount of macro-nutrients contained in {self.name} is {self.total_grams}."
+        return f"{self.name} contains {self.protein}grams of protein, {self.carbs}grams of carbs, and {self.fat}grams of fat. The total amount of macro-nutrients contained in {self.name} is {self.total_grams}, while the total calories amount to {self.total_calories}."
     
-    
+
+
+chocolate = Food("Chocolate", 8, 35, 20)
+
+print(chocolate)
